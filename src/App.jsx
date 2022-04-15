7a95cb05f86cb32e378494fa853925b1cfe0c438
Galeid <galeid475@gmail.com>
2022-04-14 22:07:54 -0500

    Tailwind added and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="container m-10 mx-auto">
+      <h1 className="text-3xl font-bold text-cyan-800">
+        Tailwind 3 With React Js
+      </h1>
+      <button className="px-6 py-2 text-sm text-white rounded shadow-xl bg-emerald-500">
+        Success
+      </button>
+    </div>
+  )
+}
+
+export default App;
